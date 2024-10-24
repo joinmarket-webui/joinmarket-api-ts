fb2f3a249a2a4928fbe046938eea0dfb64c201e2
theborakompanioni <theborakompanioni+github@gmail.com>
2024-10-24 18:37:54 +0200

    chore: response types in example

--- example.ts
+++ example.ts
@@ -1,14 +1,34 @@
+import { ErrorResponse, ResponseObjectMap } from 'openapi-typescript-helpers'
+import type { components } from './jm-wallet-rpc'
 import createClient from './index'
 
-const client = createClient({ baseUrl: 'http://localhost:3000' })
+const client = createClient({ baseUrl: 'http://localhost:3000/api/v1/' })
+
+type FetchResponse<T> =
+  | {
+      data: T
+      error?: never
+      response: Response
+    }
+  | {
+      data?: never
+      error: ErrorResponse<ResponseObjectMap<T>, 'application/json'>
+      response: Response
+    }
+
+type GetinfoResponse = FetchResponse<components["schemas"]["GetinfoResponse"]>
+
+const getinfo = async () : Promise<GetinfoResponse> => {
+  const fetchResponse = await client.GET('/getinfo')
+  return fetchResponse
+}
 
 // https://openapi-ts.dev/openapi-fetch/
 ;(async function() {
     const {
         data, // only present if 2XX response
         error, // only present if 4XX or 5XX response
-        response
-      } = await client.GET('/getinfo')
+      } = await getinfo()
 
-      console.log('/getinfo', data, error, response)
+      console.log('/getinfo', data, error)
 })()

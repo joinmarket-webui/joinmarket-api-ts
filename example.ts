afba7c59e64c02095ed38d4e04ebfa463f633729
theborakompanioni <theborakompanioni+github@gmail.com>
2024-10-25 00:06:36 +0200

    example: custom example auth middleware

--- example.ts
+++ example.ts
@@ -1,20 +1,42 @@
-import { Middleware } from 'openapi-fetch'
+import { ClientOptions, Middleware, MiddlewareCallbackParams } from 'openapi-fetch'
 import { ErrorResponse, ResponseObjectMap } from 'openapi-typescript-helpers'
 import type { components } from './src/jm-wallet-rpc/jm-wallet-rpc'
 import createClient from './src/jm-wallet-rpc/index'
 
-const middleware: Middleware = {
-  async onRequest({ request, options }) {
-    // request.headers.set('Authorization', `Bearer ${accessToken}`);
+type ApiToken = string
+
+const buildAuthHeader = (token: ApiToken): [string, string] => {
+  return ['x-jm-authorization', `Bearer ${token}`]
+}
+
+const loggingMiddleware: Middleware = {
+  async onRequest({ id, schemaPath, request } : MiddlewareCallbackParams) {
+    console.debug('[onRequest]', id, schemaPath, request.url)
     return undefined // undefined means: 'do nothing'
   },
-  async onResponse({ request, response, options }) {
+  async onResponse({ id, schemaPath, response }: MiddlewareCallbackParams & { response: Response }) {
+    console.debug('[onResponse]', id, schemaPath, response.status, response.statusText, response.ok)
     return undefined // undefined means: 'do nothing'
   },
 }
 
-const client = createClient({ baseUrl: 'http://localhost:3000/api/v1/' })
-client.use(middleware)
+const createJamAuthenticationMiddleware : (apiToken: ApiToken) => Middleware = (apiToken) => ({
+  async onRequest({ request } : MiddlewareCallbackParams) {
+    const authHeader = buildAuthHeader(apiToken)
+    request.headers.set(authHeader[0], authHeader[1])
+    return request
+  }
+})
+
+const clientOptions: ClientOptions = {
+  baseUrl: 'http://localhost:3000/api/v1/',
+}
+const client = createClient(clientOptions)
+client.use(loggingMiddleware)
+
+const jamAuthMiddleware = createJamAuthenticationMiddleware('example')
+client.use(jamAuthMiddleware) // example of registering the auth middleware
+client.eject(jamAuthMiddleware) // example of ejecting the auth middleware again (it is NOT used!)
 
 type FetchResponse<T> = {
   data?: T

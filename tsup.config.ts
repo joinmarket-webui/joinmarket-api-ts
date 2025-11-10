22450ff75cfc21204887dd8fd2edfa8c6a542b87
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-11-11 02:22:01 +0530

    remove unnecessary things

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,13 @@
 import path from 'path';
 import { defineConfig } from 'tsup';
 
-const env = process.env.NODE_ENV;
-
 export default defineConfig({
-  outDir: 'dist',
-  entry: ['src/index.ts'],
-  bundle: env === 'production',
-  clean: true,
-  dts: true,
-  format: ['cjs', 'esm'],
-  external: ['@tanstack/react-query'],
-  minify: env === 'production',
-  watch: env === 'development',
-  sourcemap: env === 'development',
-  tsconfig: path.resolve(__dirname, './tsconfig.json')
+    entry: ['src/**/*.ts'],
+    format: ['esm'],
+    dts: true,
+    clean: true,
+    bundle: false,
+    splitting: false,
+    external: ['@tanstack/react-query'],
+    tsconfig: path.resolve(__dirname, './tsconfig.json')
 });
